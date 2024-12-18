2ef1134794f2509f10a44e38cde7ec9bcf9be017
Leo Shen <58051347+Soldier4893@users.noreply.github.com>
2024-12-18 00:32:13 -0500

    add MPRP

--- main.py
+++ main.py
@@ -1,24 +1,21 @@
 import numpy as np
 from nonlincg import *
 import matplotlib.pyplot as plt
-
-if __name__ == "__main__":
-    np.random.seed(574)
-    np.seterr(all='ignore')
-
+    
+def calculations(i):
     # initialization
-    n = 20
-    maxiter = 500
-    x0 = np.random.rand(n) * 10
-
-    f, g, solution = get_objective_function(n, index = 0) # index = 0, 1, 2
+    index = i
+    n = 600
+    maxiter = 2000
+    x0 = 1.5 * np.ones(n)
+    f, g, solution = get_objective_function(n, index = index) # index = 0, 1, 2, 3, 4, 5
     mode = 'f'
 
     # plotting and algorithm
     i = 0
     e0 = 0
     plt.figure(figsize=(12, 12))
-    for beta in [AG, SD,PRP, HS, DY, N, FR, LS]:
+    for beta in [AG, SD, FR, PRP, HS, DY, LS, N, MPRP]:
         i+= 1
         if beta != AG:
             x_final, errors, (coeff, isSuperlinear) = NLCG(f, g, x0, beta, max_iter=maxiter, solution=solution, mode=mode)
@@ -26,21 +23,27 @@ if __name__ == "__main__":
             x_final, errors, (coeff, isSuperlinear) = AG(f, g, x0, max_iter=maxiter, solution=solution, mode=mode)
         
         e0 = errors[0]
-        plt.semilogy(errors, label=beta.__name__, linewidth = 2, linestyle="solid" if i < 3 else "dashed" if i < 6 else "dashdot")
+        plt.semilogy(errors, label=beta.__name__, linewidth = 2, linestyle="solid" if i < 7 else "dashed" if i < 6 else "dashdot")
         
         convergence = 'Least Squares Failed or Did not converge'
         if isSuperlinear: convergence = f'Linear, order {coeff[0]:<5.3f} rate {coeff[1]:<5.3f}' 
         elif isSuperlinear == False: convergence = f'Sublinear     1/k^{coeff[0]:<5.3f}'
         print(f"Method: {beta.__name__:<5} Final Error: {errors[-1]:<10.2e}{convergence}")
+        plt.xlabel('Iteration')
+        plt.ylabel(f'{mode}-error (log scale)')
+        plt.title(f'{mode}-error vs Iteration Number, problem {index}')
+        plt.legend()
+        plt.ylim(top=e0, bottom=1e-10)
+        plt.grid(True)
+        plt.savefig(f'{mode}_{index}_{n}.png', dpi=300)
     
-
-    # plt.semilogy([np.linalg.norm(e0)*0.851**(i**0.995) for i in range(300)], label='test', linewidth = 2, linestyle="solid" if i < 5 else "dashed")
-    # more plotting
-    plt.xlabel('Iteration')
-    plt.ylabel(f'{mode}-error (log scale)')
-    plt.title(f'{mode}-error vs Iteration Number')
-    plt.legend()
-    plt.ylim([1e-12, e0])
-    plt.grid(True)
+if __name__ == "__main__":
+    np.random.seed(574)
+    np.seterr('ignore')
+    
+    for i in range(4):
+        calculations(i)
+    
+    #plt.semilogy([e0*0.9**(i**2) for i in range(maxiter)], label='test', linewidth = 2, linestyle="solid" if i < 5 else "dashed")
+    
     plt.show()
-    
--- nonlincg.py
+++ nonlincg.py
@@ -9,33 +9,83 @@ def get_objective_function(n, index=0):
     a = np.random.rand(n,n)
     A = a.T @ a + np.eye(n)
     if index == 0: # linear equation for SPD matrix
-        solution = np.ones(n)
+        solution = 10 * np.ones(n)
         b = A @ solution
         f = lambda x: 1/2*x@A@x-b@x
         g = lambda x: A@x-b
     elif index == 1: # tikhinov regularization
         C = np.random.rand(n,n)
-        b = np.ones(n)
-        solution = np.linalg.pinv(A@A + C@C) @ A.T@b
+        b = 10 * np.ones(n)
+        solution = np.linalg.pinv(A.T@A + C.T@C) @ A.T@b
         f = lambda x: (A@x-b).T@(A@x-b) + (C@x).T @ (C@x)
-        g = lambda x: 2*A.T @ (A @ x - b) + 2 * C.T @ C @ x
-    elif index == 2: # noisy quadratic
+        g = lambda x: 2*A.T @ (A @ x - b) + 2 * (C.T @ C) @ x
+    elif index == 2: # rosenbrock
+        def f2(x):
+            total = 0
+            for i in range(n-1):
+                total += 100 * (x[i+1] - x[i]**2)**2 + (1 - x[i])**2
+            return total
+        def g2(x):
+            grad = np.zeros(n)
+            grad[0] = -400 * x[0] * (x[1] - x[0]**2) + 2 * (x[0] - 1)
+            for i in range(1, n-1):
+                grad[i] = -400 * x[i] * (x[i+1] - x[i]**2) + 2 * (x[i] - 1) + 200 * (x[i] - x[i-1]**2)
+            grad[n-1] = 200 * (x[n-1] - x[n-2]**2)
+            return grad
+        f = f2
+        g = g2
+        solution = np.ones(n)
+    elif index == 3: # tridigonal white and holst
+        def f4(x):
+            total = 0
+            for i in range(n-1):
+                total += 4*(x[i+1]-x[i]**3)**2 + (x[i]-1)**2
+            return total
+        def g4(x):
+            total = np.zeros(n)
+            for i in range(n-1):
+                total[i] = -24*x[i]**2*(x[i+1]-x[i]**3) + 2*(x[i]-1)
+            return total
+        f = f4
+        g = g4
+        solution = np.ones(n)
+    elif index == 4: # Himmelblg
+        def f3(x):
+            total = 0
+            for i in range(n//2):
+                x2i = x[2*i]
+                x2i1 = x[2*i+1]
+                total += (2*x2i**2 + 3*x2i1)*np.exp(-x2i-x2i1)
+            return total
+        def g3(x):
+            grad = np.zeros(n)
+            for i in range(n//2):
+                x2i = x[2*i]
+                x2i1 = x[2*i+1]
+                ex = np.exp(-x2i-x2i1)
+                grad[2*i] = (-x2i**2 + 4*x2i)*ex
+                grad[2*i+1] = (-3*x2i1**2 + 6*x2i1)*ex
+            return grad
+        f = f3
+        g = g3
+        solution = np.zeros(n)
+    elif index == 5: # noisy quadratic
         solution = np.ones(n)
         b = A @ solution
-        f = lambda x: 1/2*x@A@x-b@x + np.random.normal(0, 0.01)
-        g = lambda x: A@x-b+ np.random.normal(0, 0.01, n)
+        f = lambda x: 1/2*x@A@x-b@x + np.random.normal(0, 0.001)
+        g = lambda x: A@x-b+ np.random.normal(0, 0.001, n)
     else:
         raise ValueError('objective not recognized')
     return f, g, solution
 
 # Use Armijo line search to satify wolfe conditions
-def search(f, g, x, d, alpha0=10.0, rho=0.5, c1=1e-4, c2=0.9):
+def search(f, g, x, d, alpha0=10.0, rho=0.5, c1=1e-4, c2=0.1):
     alpha = alpha0
     grad = g(x)
     
-    for _ in range(800):
-        if f(x + alpha * d) <= f(x) + c1 * alpha * np.dot(grad, d) and\
-            np.dot(g(x + alpha * d), d) >= c2 * np.dot(grad, d):
+    for _ in range(1500):
+        if f(x + alpha * d) <= f(x) + c1 * alpha * np.dot(grad, d):\
+            #  and np.dot(g(x + alpha * d), d) >= c2 * np.dot(grad, d):
                 return alpha
         alpha *= rho
     return 0
@@ -48,7 +98,7 @@ def q_search(f, g, x, d, A, alpha0=None, rho=None, c1=None, c2=None):
 def NLCG(f, g, x0, compute_beta=None,  max_iter=1000, solution=None, mode = 'function'):
     def error_func(mode):
         if mode == 'f':
-            return np.linalg.norm(f(x) - f(solution))
+            return np.linalg.norm(f(x))
         elif mode == 'g':
             return np.linalg.norm(g(x))
         elif mode == 'x':
@@ -57,7 +107,7 @@ def NLCG(f, g, x0, compute_beta=None,  max_iter=1000, solution=None, mode = 'fun
             raise ValueError('errormode not recognized')
     
     # setup
-    tol = {'f': 1e-11, 'g': 1e-11, 'x': 1e-11}[mode]
+    tol = {'f': 1e-10, 'g': 1e-10, 'x': 1e-10}[mode]
     errors = tol * np.ones(max_iter)
     x = x0
     r = g(x) #residual
@@ -76,19 +126,16 @@ def NLCG(f, g, x0, compute_beta=None,  max_iter=1000, solution=None, mode = 'fun
         
         x_new = x + alpha * d
         g_new = g(x_new)
-        
-        # if k % len(x) == 0:
-        #     beta = 0
-        # else:
-        beta = compute_beta(f, r, g_new, d, x, x_new, k)
+        if k % len(x) == 0:
+            beta = 0
+        else:
+            beta = compute_beta(f, r, g_new, d, x, x_new, k)
         if np.isnan(beta):
             counter = k
             errors = errors[:counter]
             break
         d_new = -g_new + beta * d
         
-        
-
         x = x_new
         r = g_new
         d = d_new
@@ -112,17 +159,18 @@ def calculate_rate(errors, M):
     
 
     # sublinear convergence, estimate p for convergence rate C/k^p
-    if q < 0.97 or c >= 1:
+    if q < 0.99 or c >= 1:
         y1 = np.log(relevant)
         x1 = np.log(np.arange(1,M+1))
         coeff = np.polyfit(-x1, y1, 1)
+        coeff[1] = np.e**coeff[1]
         return coeff, False
     return coeff, True
 
-def ag_search(f, g, y, alpha0=10.0, rho=0.5):
+def ag_search(f, g, y, alpha0=10.0, rho=0.4):
     grad = g(y)
     alpha = alpha0
-    for _ in range(100):
+    for _ in range(800):
         if f(y - alpha * grad) <= f(y) - alpha*np.dot(grad, grad)/2:
             return alpha
         alpha *= rho
@@ -141,7 +189,7 @@ def AG(f, g, x0, max_iter=1000, solution=None, mode='function'):
             raise ValueError('errormode not recognized')
     
     # setup
-    tol = {'f': 1e-11, 'g': 1e-8, 'x': 1e-8}[mode]
+    tol = {'f': 1e-10, 'g': 1e-10, 'x': 1e-10}[mode]
     errors = tol * np.ones(max_iter)
     x = x0
     y = x0
@@ -151,6 +199,7 @@ def AG(f, g, x0, max_iter=1000, solution=None, mode='function'):
     for k in range(max_iter):
         if error_func(mode) < tol:
             counter = k
+            errors = errors[:counter]
             break
         alpha = ag_search(f, g, y)
 
@@ -169,8 +218,6 @@ def FR(f, g, g_new, d, x, x_new, k):
 def PRP(f, g, g_new, d, x, x_new, k):
     return np.dot(g_new, g_new-g) / np.linalg.norm(g)**2
 def HS(f, g, g_new, d, x, x_new, k):
-    if k % len(x) == 0:
-        return 0
     return np.dot(g_new, g_new - g)/np.dot(d, g_new - g)
 def DY(f, g, g_new, d, x, x_new, k):
     return np.dot(g_new, g_new)/np.dot(d, g_new-g)
@@ -182,38 +229,34 @@ def LS(f, g, g_new, d, x, x_new, k):
     return -np.dot(g_new, g_new-g)/np.dot(d, g)
 def CD(f, g, g_new, d, x, x_new, k):
     return -np.dot(g_new, g_new)/np.dot(d,g)
+def MPRP(f, g, g_new, d, x, x_new, k):
+    y = g_new - g
+    ynorm = np.linalg.norm(y)
+    gnorm = np.linalg.norm(g)
+    nu = 0.8
+    kappa = 10
+    l = np.dot(g_new, y - nu*ynorm**2*d/gnorm**2)
+    a = max(0, l)/gnorm**2
+    b = kappa * np.linalg.norm(g_new)**2 / gnorm**2
+    return min(a, b)
 def MAD(f, g, g_new, d, x, x_new, k):
     n = len(x)
-    mu = 0.99
+    mu = 1e-4
     mu2 = mu**2
     eta = np.dot(d,g_new)
     zeta = np.dot(d,d)*np.dot(g_new,g_new)
     term = (eta-mu*zeta)*(eta+mu*zeta)
     beta = 0
 
-    l = 2*np.random.randint(0,2)-1
+    l = 2*np.random.rand()-1
     if eta == 0:
-        beta = l*np.sqrt(abs(1-mu2))*np.dot(g_new, g_new)/(mu*zeta)
+        beta = l*np.sqrt(abs(1-mu2))/(mu*zeta)
     elif term == 0:
-        beta = np.dot(g_new, g_new)/(2*eta)
+        beta = l/(2*eta)
     else:
         numer = (1-mu2)*eta+ l*mu*zeta*np.sqrt(abs((1-mu2)*(1-eta/zeta)*(1+eta/zeta)))
-        beta = np.dot(g_new, g_new)*numer/term    
-    return l * beta
+        beta = numer/term
+    return np.dot(g_new, g_new)*beta
 def SD(f, g, g_new, d, x, x_new, k):
     return 0
 
-def rosenbrock(x):
-    m = len(x)
-    sum = 0
-    for i in range(m-1):
-        sum += 100 * (x[i+1] - x[i]**2)**2 + (1 - x[i])**2
-    return sum
-def rosenbrock_grad(x):
-    m = len(x)
-    grad = np.zeros(m)
-    grad[0] = -400 * x[0] * (x[1] - x[0]**2) + 2 * (x[0] - 1)
-    for i in range(1, m-1):
-        grad[i] = -400 * x[i] * (x[i+1] - x[i]**2) + 2 * (x[i] - 1) + 200 * (x[i] - x[i-1]**2)
-    grad[m-1] = 200 * (x[m-1] - x[m-2]**2)
-    return grad
